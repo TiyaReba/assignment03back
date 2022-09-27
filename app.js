86e588f452ed4410784c0a66d7d3b9aed16dffbe
TiyaReba <105912721+TiyaReba@users.noreply.github.com>
2022-09-28 00:43:25 +0530

    app.use(express.static(path.join(__dirname, 'public')));

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(bodyparser.json());
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 // console.log("body"+ req.body);
 app.post('/signup',function(req,res){
     res.header("Access-Control-Allow-Origin",'*');
